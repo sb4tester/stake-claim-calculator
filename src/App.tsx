cf258719a58683238e7117199eac50864c7e600b
nogira <83847986+nogira@users.noreply.github.com>
2022-05-22 21:50:04 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { Space, Select, Title, NumberInput, Paper } from '@mantine/core';
+
+import { rewardsToWithdrawAt } from './calculateRewards.js';
+
+
+function InputText(props: any) {
+  const [value, setValue, label] = [props.val, props.set, props.label];
+  return (
+    <Select 
+      label={label}
+      value={value}
+      onChange={setValue}
+      data={[ "Cosmos", "Sentinel"]}
+    />
+  );
+}
+
+function App() {
+  const [chain, setChain] = useState("Custom");
+  const [claimFee, setClaimFee] = useState(0);
+  const [stakeFee, setStakeFee] = useState(0);
+  const [totalStaked, setTotalStaked] = useState(0);
+  const [claimAt, setClaimAt] = useState(0);
+
+  useEffect(() => {
+    switch(chain) {
+      case "Custom":
+        break;
+      case "Cosmos":
+        setClaimFee(0.0014);
+        setStakeFee(0.0025);
+        break;
+      case "Sentinel":
+        setClaimFee(0.014);
+        setStakeFee(0.025);
+        break;
+    }
+    const totalFee = claimFee + stakeFee;
+    if (totalFee > 0 && totalStaked > 0) {
+      const num = rewardsToWithdrawAt(totalStaked, totalFee);
+      // round to 6 decimal places
+      const rounded = Math.round(num * 1000000) / 1000000;
+      setClaimAt(rounded);
+      console.log(claimAt);
+    }
+  })
+  
+
+  return (
+    <Paper shadow="xs" p="md" className="App" >
+      <Select 
+        label="Blockchain Fees"
+        value={chain}
+        onChange={setChain}
+        data={["Custom","Cosmos", "Sentinel", "Terra"]}
+      />
+
+      <NumberInput
+        label="Claim Fee"
+        precision={6}
+        required
+        value={claimFee}
+        onChange={(val: number) => setClaimFee(val)}
+      />
+
+      <NumberInput
+        label="Stake Fee"
+        precision={6}
+        required
+        value={stakeFee}
+        onChange={(val: number) => setStakeFee(val)}
+      />
+
+      <NumberInput
+        label="Total Staked"
+        precision={6}
+        required
+        value={totalStaked}
+        onChange={(val: number) => setTotalStaked(val)}
+      />
+
+      <Space h={20} />
+
+      <Title order={4}>{"Claim At"}</Title>
+      <Title order={1}>{claimAt}</Title>
+      
+      {/* <button type="button" onClick={() => setCount((count) => count + 1)}>
+        count is: {count}
+      </button> */}
+    </Paper>
+  )
+}
+
+export default App
---
+++ src/calculateRewards.js
@@ -0,0 +1,45 @@
+/*
+we are trying to find the percentage at 0 compound that gives the highest 
+long-term return
+
+constant: stakedBal, fee
+variable: percentage
+
+0 compound:
+  finalBal = stakedBal * (1 + percentage) - fee
+1 compound:
+  finalBal = (stakedBal * (1 + (percentage / 2)) - fee) * (1 + (percentage / 2)) - fee
+
+when they equal each other, it means the 0 compound is close to optimal, as
+1 compound offers no benefit over 0 compound
+
+stakedBal * (1 + percentage) - fee = (stakedBal * (1 + (percentage / 2)) - fee) * (1 + (percentage / 2)) - fee
+stakedBal * (1 + percentage) = (stakedBal * (1 + (percentage / 2)) - fee) * (1 + (percentage / 2))
+https://www.wolframalpha.com/input?i=x+*+%281+%2B+z%29+%3D+%28x+*+%281+%2B+%28z+%2F+2%29%29+-+y%29+*+%281+%2B+%28z+%2F+2%29%29%2C+rearrange+for+z
+  x * (1 + z) = (x * (1 + (z / 2)) - y) * (1 + (z / 2)), rearrange for z
+  => z = (y ± sqrt(y(4x + y))) / x, x ≠ 0
+  => percentage = (fee ± sqrt(fee * ((4 * stakedBal) + fee))) / stakedBal, stakedBal ≠ 0
+*/
+// console.log((fee + Math.sqrt(fee * ((4 * stakedBal) + fee))) / stakedBal)
+// console.log((fee - Math.sqrt(fee * ((4 * stakedBal) + fee))) / stakedBal)
+/*
+negative percentage doesnt make sense, so final equation is:
+  percentage = (fee + Math.sqrt(fee * ((4 * stakedBal) + fee))) / stakedBal
+
+testing the percentages of both 0 and 1 compound (percentage and percentage/2),
+they both give the same returns over time, as expected, however, when you test 
+the percentage in between (percentage * 3/4), it gives slightly better returns, 
+though EXTREMELY minimal improvement over 0 compound. thus instead of 0 compound
+percentage, we use percentage * 3/4
+
+*/
+
+export function rewardsToWithdrawAt(stakedBal, fee) {
+    const percentage = 3/4 * (fee + Math.sqrt(fee * ((4 * stakedBal) + fee))) / stakedBal;
+    console.log(percentage);
+    const withdrawAt = stakedBal * percentage
+    console.log(withdrawAt);
+    return withdrawAt;
+}
+
+rewardsToWithdrawAt(5.09, 0.002847)
